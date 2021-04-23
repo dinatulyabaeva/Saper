2d19441da6926af9aff900da8ee0271050a4235a
Дина Тулябаева <79083810+dinatulyabaeva@users.noreply.github.com>
2021-04-23 22:37:03 +0300

    Add files via upload
    omg! кажется что-то получилось, не зря плакала..

---
+++ Saper.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace saper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.новаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.уровеньСложностиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.легкийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.среднийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сложныйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel = new System.Windows.Forms.Panel();
+            this.richTextBox_for_scores = new System.Windows.Forms.RichTextBox();
+            this.menuStrip.SuspendLayout();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаяИграToolStripMenuItem,
+            this.уровеньСложностиToolStripMenuItem,
+            this.справкаToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(783, 30);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // новаяИграToolStripMenuItem
+            // 
+            this.новаяИграToolStripMenuItem.Name = "новаяИграToolStripMenuItem";
+            this.новаяИграToolStripMenuItem.Size = new System.Drawing.Size(103, 26);
+            this.новаяИграToolStripMenuItem.Text = "Новая игра";
+            this.новаяИграToolStripMenuItem.Click += new System.EventHandler(this.новаяИграToolStripMenuItem_Click);
+            // 
+            // уровеньСложностиToolStripMenuItem
+            // 
+            this.уровеньСложностиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.легкийToolStripMenuItem,
+            this.среднийToolStripMenuItem,
+            this.сложныйToolStripMenuItem});
+            this.уровеньСложностиToolStripMenuItem.Name = "уровеньСложностиToolStripMenuItem";
+            this.уровеньСложностиToolStripMenuItem.Size = new System.Drawing.Size(162, 26);
+            this.уровеньСложностиToolStripMenuItem.Text = "Уровень сложности";
+            // 
+            // легкийToolStripMenuItem
+            // 
+            this.легкийToolStripMenuItem.Name = "легкийToolStripMenuItem";
+            this.легкийToolStripMenuItem.Size = new System.Drawing.Size(158, 26);
+            this.легкийToolStripMenuItem.Text = "Легкий";
+            this.легкийToolStripMenuItem.Click += new System.EventHandler(this.легкийToolStripMenuItem_Click);
+            // 
+            // среднийToolStripMenuItem
+            // 
+            this.среднийToolStripMenuItem.Name = "среднийToolStripMenuItem";
+            this.среднийToolStripMenuItem.Size = new System.Drawing.Size(158, 26);
+            this.среднийToolStripMenuItem.Text = "Средний";
+            this.среднийToolStripMenuItem.Click += new System.EventHandler(this.среднийToolStripMenuItem_Click);
+            // 
+            // сложныйToolStripMenuItem
+            // 
+            this.сложныйToolStripMenuItem.Name = "сложныйToolStripMenuItem";
+            this.сложныйToolStripMenuItem.Size = new System.Drawing.Size(158, 26);
+            this.сложныйToolStripMenuItem.Text = "Сложный";
+            this.сложныйToolStripMenuItem.Click += new System.EventHandler(this.сложныйToolStripMenuItem_Click);
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(81, 26);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            this.справкаToolStripMenuItem.Click += new System.EventHandler(this.справкаToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(67, 26);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.richTextBox_for_scores);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel.Location = new System.Drawing.Point(567, 30);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(216, 548);
+            this.panel.TabIndex = 1;
+            // 
+            // richTextBox_for_scores
+            // 
+            this.richTextBox_for_scores.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.richTextBox_for_scores.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox_for_scores.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox_for_scores.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox_for_scores.Name = "richTextBox_for_scores";
+            this.richTextBox_for_scores.ReadOnly = true;
+            this.richTextBox_for_scores.Size = new System.Drawing.Size(216, 548);
+            this.richTextBox_for_scores.TabIndex = 0;
+            this.richTextBox_for_scores.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(783, 578);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "Minesweeper by Dina";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem новаяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem уровеньСложностиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem легкийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem среднийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сложныйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.RichTextBox richTextBox_for_scores;
+    }
+}
+
---
+++ Saper.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saper
+{
+    public partial class MainForm : Form
+    {
+        int offset = 40;
+        int width = 9;
+        int height = 9;
+        bool isFirstClick = true;
+        int cellsOpened = 0;
+        int bombs = 0;
+        int bombPercent;
+        FieldButton[,] field;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            field = new FieldButton[width, height];
+            //GenerateField();
+        }
+        
+        void FieldButtonClick(object sender, MouseEventArgs e)
+        {
+
+            FieldButton clickedButton = (FieldButton)sender;
+            if (e.Button == MouseButtons.Left && clickedButton.isClickable)  // новое
+            {
+                if (clickedButton.isBomb)
+                {
+                    if (isFirstClick)
+                    {
+                        clickedButton.isBomb = false;
+                        isFirstClick = false;
+                        bombs--;
+                        OpenRegion(clickedButton.xCoord, clickedButton.yCoord, clickedButton);
+                    }
+                    else
+                    {
+                        Explode();
+                    }
+
+                }
+                else
+                {
+                    EmptyFieldButtonClick(clickedButton);
+                }
+                isFirstClick = false;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                clickedButton.isClickable = !clickedButton.isClickable;
+                if (!clickedButton.isClickable)
+                {
+                    clickedButton.Image = Image.FromFile("C:/Users/пк/Desktop/з.png");
+                }
+                else
+                {
+                    clickedButton.Image = Image.FromFile("");
+                }
+            }
+            CheckWin();
+
+        }
+        void Explode()
+        {
+            foreach (FieldButton button in field)
+            {
+                if (button.isBomb)
+                {
+                    button.Image = Image.FromFile("C:/Users/пк/Desktop/4.png");
+                }
+            }
+            MessageBox.Show("Увы....");
+            //Application.Restart();
+        }
+        void EmptyFieldButtonClick(FieldButton clickedButton)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (field[x, y] == clickedButton)
+                    {
+                        //bombsAround = CountBombsAround(x, y);
+                        OpenRegion(x, y, clickedButton);
+                    }
+                }
+            }
+        }
+        void OpenRegion(int xCoord, int yCoord, FieldButton clickedButton)
+        {
+            Queue<FieldButton> queue = new Queue<FieldButton>();
+            queue.Enqueue(clickedButton);
+            clickedButton.wasAdded = true;
+            while (queue.Count > 0)
+            {
+                FieldButton currentCell = queue.Dequeue();
+                //int bombsAround = CountBombsAround(x, y);
+                OpenCell(currentCell.xCoord, currentCell.yCoord, currentCell);
+                cellsOpened++;
+                if (CountBombsAround(currentCell.xCoord, currentCell.yCoord) == 0)
+                {
+                    for (int j = currentCell.yCoord - 1; j <= currentCell.yCoord + 1; j++) //поменять
+                    {
+                        for (int i = currentCell.xCoord - 1; i <= currentCell.xCoord + 1; i++)
+                        {
+                            if (i == currentCell.xCoord && j == currentCell.yCoord)//
+                            {
+                                continue;
+                            }
+                            if (i >= 0 && i < width && j < height && j >= 0)
+                            {
+                                if (!field[i, j].wasAdded)
+                                {
+                                    queue.Enqueue(field[i, j]);
+                                    field[i, j].wasAdded = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        void OpenCell(int x, int y, FieldButton clickedButton)
+        {
+            int bombsAround = CountBombsAround(x,y);
+            if (bombsAround == 0)
+            {
+
+            }
+            else
+            {
+                clickedButton.Text = "" + bombsAround;
+            }
+            clickedButton.Enabled = false;
+        }
+        int CountBombsAround(int xB, int yB)
+        {
+            int bombsAround = 0;
+            for (int x = xB - 1; x <= xB + 1; x++)
+            {
+                for (int y = yB - 1; y <= yB + 1; y++)
+                {
+                    if (x >= 0 && x < width && y >= 0 && y < height)
+                    {
+                        if (field[x, y].isBomb == true)
+                        {
+                            bombsAround++;
+                        }
+                    }
+                }
+            }
+            return bombsAround;
+        }
+        void CheckWin()
+        {
+            int cells = width * height;
+            int emptyCells = cells - bombs;
+            if (cellsOpened >= emptyCells)
+            {
+                MessageBox.Show("Победа!");
+            }
+        }
+
+        private void новаяИграToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("всем приветики, это сапер от Дины");
+        }
+
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+            /*public void GenerateField()
+            {
+                Random random = new Random();
+                for (int j = 0; j < height; j++)
+                {
+                    for (int i = 0; i < width; i++)
+                    {
+                        FieldButton newbutton = new FieldButton();
+                        newbutton.Location = new Point(i * offset, j * offset);
+                        newbutton.Size = new Size(offset, offset);
+                        newbutton.isClickable = true;
+                        if (random.Next(0, 100) <= bombPercent)
+                        {
+                            newbutton.isBomb = true;
+                        }
+                        newbutton.xCoord = i;
+                        newbutton.yCoord = j;
+                        Controls.Add(newbutton);
+                        newbutton.MouseUp += new MouseEventHandler(FieldButtonClick);
+                        field[i, j] = newbutton;
+                    }
+                }
+            }*/
+
+        private void легкийToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    FieldButton newbutton = new FieldButton();
+                    newbutton.Location = new Point(i * offset, j * offset);
+                    newbutton.Size = new Size(offset, offset);
+                    newbutton.isClickable = true;
+                    if (random.Next(0, 100) <= 2)
+                    {
+                        newbutton.isBomb = true;
+                    }
+                    newbutton.xCoord = i;
+                    newbutton.yCoord = j;
+                    Controls.Add(newbutton);
+                    newbutton.MouseUp += new MouseEventHandler(FieldButtonClick);
+                    field[i, j] = newbutton;
+                }
+            }
+            
+        }
+
+        private void среднийToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    FieldButton newbutton = new FieldButton();
+                    newbutton.Location = new Point(i * offset, j * offset);
+                    newbutton.Size = new Size(offset, offset);
+                    newbutton.isClickable = true;
+                    if (random.Next(0, 100) <= 20)
+                    {
+                        newbutton.isBomb = true;
+                    }
+                    newbutton.xCoord = i;
+                    newbutton.yCoord = j;
+                    Controls.Add(newbutton);
+                    newbutton.MouseUp += new MouseEventHandler(FieldButtonClick);
+                    field[i, j] = newbutton;
+                }
+            }
+        }
+
+        private void сложныйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    FieldButton newbutton = new FieldButton();
+                    newbutton.Location = new Point(i * offset, j * offset);
+                    newbutton.Size = new Size(offset, offset);
+                    newbutton.isClickable = true;
+                    if (random.Next(0, 100) <= 50)
+                    {
+                        newbutton.isBomb = true;
+                    }
+                    newbutton.xCoord = i;
+                    newbutton.yCoord = j;
+                    Controls.Add(newbutton);
+                    newbutton.MouseUp += new MouseEventHandler(FieldButtonClick);
+                    field[i, j] = newbutton;
+                }
+            }
+        }
+    }
+    class FieldButton : Button
+    {
+        public bool isBomb;
+        public bool isClickable;
+        public bool wasAdded;
+        public int xCoord;
+        public int yCoord;
+    }
+}
